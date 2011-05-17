dde6a38c5bb2b0b60b968bf8ab634195d2c34197
Hannes Roest <you@example.com>
2011-05-17 16:32:21 +0200

    added genome tables

--- code/ppure/tutorial.py
+++ code/ppure/tutorial.py
@@ -8,13 +8,24 @@
 import MySQLdb
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.databases.mysql import MSEnum
+from sqlalchemy.types import Binary
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relation, backref
 #declarative
 from sqlalchemy.ext.declarative import declarative_base
-def connect():
-    return MySQLdb.connect( read_default_file='~/.my.cnf')
 
-db = create_engine('mysql://', creator=connect)
+def connection_creator(db):
+    #this fxn will return a fxn "connect" which returns a database connector
+    def connect():
+        return MySQLdb.connect( read_default_file='~/.my.cnf', db=db)
+    return connect
+
+db = create_engine('mysql://', creator=connection_creator('ddb'), echo = False)
 db.echo = False  
+db_genome = create_engine('mysql://', creator=connection_creator('ddbGenome'), echo = False)
+db_genome.echo = False  
+
 Base = declarative_base()
 class Peptide(Base):
     __tablename__ = 'peptide'
@@ -41,8 +52,103 @@ class Peptide(Base):
 Session = sessionmaker(bind=db)
 session = Session()
 
+GenomeBase = declarative_base()
+class Genome(GenomeBase):
+    __tablename__ = 'genome'
+    #
+    id             = Column('id',             Integer, primary_key=True)
+    experiment_key = Column('experiment_key', Integer)
+    gi             = Column('gi',             Integer)
+    ref            = Column('ref',            String)
+    description    = Column('description',    String)
+    comment        = Column('comment',        String)
+    insert_date    = Column('insert_date',    Date)
+    timestamp      = Column('timestamp',      TIMESTAMP)
+    #
+    #features = relation('Feature', order_by='Feature.id', backref="feature")
+    #
+    def __init__(self, **kwargs): 
+        # we go through all arguments given as keywords and set them 
+        # e.g. if init was called with (id=someid) we set here self.id = someid
+        for k in kwargs:
+            setattr( self, k, kwargs[k])
+    def __repr__(self):
+       return "<Genome('%s',id '%s', '%s')>" % (self.experiment_key, self.id, self.description )
+
+class Feature(GenomeBase):
+    __tablename__ = 'feature'
+    #
+    id           = Column('id',             Integer, primary_key=True)
+    genome_key   = Column('genome_key', Integer , ForeignKey('genome.id') )
+    sequence_key = Column('sequence_key',             Integer)
+    start        = Column('start',             Integer)
+    end          = Column('end',             Integer)
+    strand       = Column('strand',             Integer)
+    type         = Column('type',             String)
+    pseudo       = Column('pseudo',            MSEnum('yes','no') )
+    mol_type     = Column('mol_type',    String)
+    db_xref      = Column('db_xref',        String)
+    gene         = Column('gene',        String)
+    locus_tag    = Column('locus_tag',        String)
+    note         = Column('note',        String)
+    codon_start  = Column('codon_start',        String)
+    transl_table = Column('transl_table',        String)
+    product      = Column('product',        String)
+    protein_id   = Column('protein_id',        String)
+    ec_number    = Column('ec_number',        String)
+    gene_synonym = Column('gene_synonym',        String)
+    function     = Column('function',        String)
+    pan          = Column('pan',         MSEnum('yes','no') )
+    insert_data  = Column('insert_date',    Date)
+    timestamp    = Column('timestamp',      TIMESTAMP)
+    #
+    genome = relation('Genome', order_by='Genome.id', backref=backref("features", order_by=id)) 
+    #
+    def __init__(self, **kwargs): 
+        # we go through all arguments given as keywords and set them 
+        # e.g. if init was called with (id=someid) we set here self.id = someid
+        for k in kwargs:
+            setattr( self, k, kwargs[k])
+    def __repr__(self):
+       return "<Feature(seq '%s', genome '%s', '%s', '%s', '%s')>" % (self.genome_key, self.sequence_key, self.start, self.end, self.gene )
+
+
+
+class GenomeSeq(GenomeBase):
+    __tablename__ = 'genomeSeq'
+    #
+    id           = Column('id',  Integer, ForeignKey('genome.id'), primary_key=True, )
+    compress_seq = Column('compress_seq', Binary)
+    #
+    genome = relation('Genome', order_by='Genome.id', backref=backref("genomeSeq", uselist=False)) 
+    #
+    def __init__(self, **kwargs): 
+        # we go through all arguments given as keywords and set them 
+        # e.g. if init was called with (id=someid) we set here self.id = someid
+        for k in kwargs:
+            setattr( self, k, kwargs[k])
+    def __repr__(self):
+       return "<Genome Sequence(id '%s')>" % (self.id)
+
+
+
+
+genomeSession = sessionmaker(bind=db_genome)
+genomesession = genomeSession()
+
 #querying
 mypeptide = session.query(Peptide).filter_by(id='11861373').first() 
+mygenome = genomesession.query(Genome).filter_by(id='1').first() 
+myfeature = genomesession.query(Feature).filter_by(id='1').first() 
+mygseq = genomesession.query(GenomeSeq).filter_by(id='1').first() 
+
+print myfeature
+print myfeature.genome
+print 'len', len(myfeature.genome.features)
+print mygseq 
+print mygseq.genome
+print mygseq.genome.genomeSeq
+
 
 #creating
 other_peptide = Peptide(3331, 'test', 'PEPTIDEE', 0, 0, 0)
