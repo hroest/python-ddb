c1d5ac5d56ae44827a4ee1341df01f5b6a84332e
Hannes Roest <you@example.com>
2011-05-18 14:56:54 +0200

    added decorators

--- code/ppure/objects.py
+++ code/ppure/objects.py
@@ -4,11 +4,68 @@ from sqlalchemy import select
 from tables import *
 import datetime
 
+def add_init(myclass):
+
+    def init_standard(self, **kwargs): 
+        # we go through all arguments given as keywords and set them 
+        # e.g. if init was called with (id=someid) we set here self.id = someid
+        for k in kwargs:
+            setattr( self, k, kwargs[k])
+
+    def init_with_defaults(self, **kwargs): 
+        for d in self.__class__.__defaults: setattr( self, d, self.__class__.__defaults[d])
+        self.insert_date = datetime.datetime.utcnow().strftime('%Y-%m-%d')
+
+        # we go through all arguments given as keywords and set them 
+        # e.g. if init was called with (id=someid) we set here self.id = someid
+        for k in kwargs:
+            setattr( self, k, kwargs[k])
+
+    print "setting up init"
+    print myclass.__dict__
+    if myclass.__dict__.has_key('__defaults'): myclass.__init__ = init_with_defaults
+    else:                                      myclass.__init__ = init_standard
+    return myclass
+
+def add_addfxn(myclass):
+
+    #generic addignore
+    def addignore_setid(self, session):
+        p = self.exists(session)
+        if p is None: return self.add(session)
+        else: return p
+
+    #generic exist, will only check whether this id is already present
+    def exists(self, session):
+        return session.query(self.__class__).filter_by(id=self.id).first()
+
+    #generic add, will just use session
+    def add(self, session):
+        session.add(self)
+        session.commit()
+        return self
+
+    if not myclass.__dict__.has_key('add'):             myclass.add = add
+    if not myclass.__dict__.has_key('exists'):          myclass.exists = exists
+    if not myclass.__dict__.has_key('addignore_setid'): myclass.addignore_setid = addignore_setid
+
+    return myclass
+
+def add_mapper(table):
+    def wrap(myclass):
+        mapper(myclass, table)
+        return myclass
+    return wrap
+
+@add_mapper(genome_table)
+@add_addfxn
+@add_init
 class Genome(object): 
     pass
 
-mapper(Genome, genome_table)
-
+@add_mapper(genomeSeq_table)
+@add_addfxn
+@add_init
 class GenomeSeq(object): 
     def _get_seq(self):
         proxy = db_genome.execute( 'select uncompress(compress_seq) from \
@@ -17,38 +74,10 @@ class GenomeSeq(object):
     def _set_seq(self, text):
         self.compress_seq = func.compress(text)
 
-mapper(GenomeSeq, genomeSeq_table)
-
+@add_mapper(sequence_table)
+@add_addfxn
+@add_init
 class Sequence(object): 
-
-    def __init__(self, *args, **kwargs): 
-        #self._init(*args, **kwargs)
-        #def _init(self, *args, **kwargs): 
-
-        #  # We assume that an object of the same type was given as an argument
-        #  # and copy all its columns.
-        #  for arg in args:
-        #      #
-        #      # This is a little hack to determine whether the object has a
-        #      # _sa_class_manager which we assume it has when it has a
-        #      # _sa_instance_state
-        #      if arg.__dict__.has_key('_sa_instance_state'):
-        #          #try
-        #          #except
-        #          # self.sequence = arg.sequence
-        #          # self.sha1 = arg.sha1
-        #          # self.id = arg.id
-        #          tt = ['sequence', 'sha1', 'proteins']
-        #          #tt = arg._sa_class_manager.keys()
-        #          #for k in arg._sa_class_manager.keys():
-        #          for k in tt:
-        #              self.__setattr__( k, arg.__getattribute__( k ) ) 
-
-        # we go through all arguments given as keywords and set them 
-        # e.g. if init was called with (id=someid) we set here self.id = someid
-        # Note that kwargs override the copy-args from above
-        for k in kwargs:
-            setattr( self, k, kwargs[k])
 
     def addignore_setid(self, session, db):
         cursor = db.cursor()
@@ -74,33 +103,25 @@ class Sequence(object):
         else: rseq = self.sequence
         return "<Sequence(id '%s', '%s', sequence '%s')>" % (self.id, self.sha1, rseq)
 
-
-mapper(Sequence, sequence_table)
-
+@add_init
+@add_addfxn
 class Feature(object): 
 
-    def __init__(self, **kwargs): 
-        defaults = { 
-            'mol_type'      : '',
-            'db_xref'       : '',   
-            'gene'          : '',   
-            'locus_tag'     : '',   
-            'note'          : '',   
-            'codon_start'   : '',   
-            'transl_table'  : '',   
-            'product'       : '',   
-            'protein_id'    : '',   
-            'ec_number'     : '',   
-            'gene_synonym'  : '',   
-            'function'      : '',   
-        }
-        for d in defaults: setattr( self, d, defaults[d])
-        self.insert_date = datetime.datetime.utcnow().strftime('%Y-%m-%d')
+    ___defaults = { 
+        'mol_type'      : '',
+        'db_xref'       : '',   
+        'gene'          : '',   
+        'locus_tag'     : '',   
+        'note'          : '',   
+        'codon_start'   : '',   
+        'transl_table'  : '',   
+        'product'       : '',   
+        'protein_id'    : '',   
+        'ec_number'     : '',   
+        'gene_synonym'  : '',   
+        'function'      : '',   
+    }
 
-        # we go through all arguments given as keywords and set them 
-        # e.g. if init was called with (id=someid) we set here self.id = someid
-        for k in kwargs:
-            setattr( self, k, kwargs[k])
     def __repr__(self):
        return "<Feature(id '%s', genome '%s', seq '%s', '%s', '%s', '%s', '%s', '%s')>" % (self.id, self.genome_key, self.sequence_key, self.strand, self.type, self.start, self.end, self.gene )
 
@@ -109,41 +130,26 @@ mapper(Feature, feature_table , properties = {
         feature_table.c.sequence_key==sequence_table.c.id, 
         foreign_keys=[feature_table.c.sequence_key]) } )
  
-
+@add_mapper(peptide_table)
+@add_addfxn
+@add_init
 class Peptide(object):
-    def __init__(self, **kwargs): 
-        # we go through all arguments given as keywords and set them 
-        # e.g. if init was called with (id=someid) we set here self.id = someid
-        for k in kwargs:
-            setattr( self, k, kwargs[k])
     def __repr__(self):
        return "<Peptide('%s','%s','%s')>" % (self.id, self.experiment_key, self.sequence )
-mapper(Peptide, peptide_table)
 
+    def exists(self, session):
+        return session.query(self.__class__).filter_by(sequence=self.sequence, experiment_key=self.experiment_key).first()
+
+@add_mapper(gene_table)
+@add_addfxn
+@add_init
 class Gene(object):
     def __repr__(self):
        return "<Gene('%s','%s','%s')>" % (self.id, self.experiment_key, self.description )
-mapper(Gene, gene_table)
 
+@add_addfxn
+@add_init
 class Protein(object):
-
-    def __init__(self, **kwargs): 
-        defaults = { 'probability' : 0 , 'comment' : '', 'protein_type' : '', 'file_key' : 0, 'parse_key' : 0, 'gene_key' : 0}
-        for d in defaults: setattr( self, d, defaults[d])
-        self.insert_date = datetime.datetime.utcnow().strftime('%Y-%m-%d')
-
-        # we go through all arguments given as keywords and set them 
-        # e.g. if init was called with (id=someid) we set here self.id = someid
-        for k in kwargs:
-            setattr( self, k, kwargs[k])
-
-    def addignore_setid(self, session):
-        p = self.exists(session)
-        if p is None:
-            session.add(self)
-            session.commit()
-            return self
-        else: return p
 
     def exists(self, session):
         return session.query(Protein).filter_by(experiment_key=self.experiment_key, sequence_key=self.sequence_key).first()
@@ -183,7 +189,5 @@ mapper(Protein, protein_table, properties = {
         secondaryjoin=and_(geneProtLink_table.c.gene_key==gene_table.c.id),
         backref='proteins', 
         foreign_keys=[geneProtLink_table.c.protein_key, geneProtLink_table.c.gene_key]),
-}, 
-
-
+    }, 
 )
